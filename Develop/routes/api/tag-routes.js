e4fece1151f6cded0ab94f61797e6d588309a014
j b <j.is.brown@gmail.com>
2024-07-30 22:11:39 -0500

    routing for tag-routes.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,6 +4,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    const Tags = await Tag.findAll({
+      include: [{
+        model: Product,
+        through: ProductTag
+      }]
+    });
+    res.status(200).json(Tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all tags
   // be sure to include its associated Product data
 });
@@ -11,18 +22,67 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const aTag = await Tag.findByPk(req.params.id, {
+      include: [{
+        model: Product,
+        through: ProductTag
+      }]
+    });
+    if (!aTag) {
+      res.status(404).json({ message: "oops, try again" });
+      return;
+    }
+    res.status(200).json(aTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json({ message: "oops, try again" });
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!updatedTag) {
+      res.status(404).json({ message: "oops, try again" });
+      return;
+    }
+    res.status(200).json(updatedTag)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deleteTag) {
+      res.status(404).json({ message: "oops, try again" });
+      return;
+    }
+    res.status(200).json(deleteTag)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
